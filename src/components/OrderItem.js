f1288e887669857bf4564cc93e216dc155bb2bd3
ffraniel <francis.whitehead@googlemail.com>
2019-04-18 18:08:46 +0100

    moved check availability on per order instead of per item

--- src/components/OrderItem.js
+++ src/components/OrderItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './OrderItem.css';
 import OrdersList from './OrdersList';
 import AmendItem from './AmendItem';
@@ -15,6 +15,19 @@ const OrderItem = ({
     handleAmendValueChange
   }) => {
 
+    const [eggsAvailable, updateEggsAvailable] = useState(null);
+
+    const checkOrderPossible = (number) => {
+        return fetch('https://wt-68dc6486277619b05f4ee73ad2a8a48e-0.sandbox.auth0-extend.com/egg-store-be/quantity')
+          .then(res => res.json())
+          .then(res => {
+            updateEggsAvailable(res.total);
+          })
+          .catch(err => {
+            console.log({error: err});
+          });
+    };
+
     const removeItemProcess = (id) => {
       let box = document.getElementById(id);
       box.classList.add('fade');
@@ -23,15 +36,21 @@ const OrderItem = ({
       }, 800)
     };
     const totalCost = order.totalCost ? order.totalCost : order.order.reduce((acc, curr)=>{
-      return (acc + (curr.price * curr.quantity)).toFixed(2);
+      return acc + (curr.price * curr.quantity);
+    }, 0);
+
+    const requiredEggs = order.order.reduce((acc, curr) => {
+      return acc + (curr.number * curr.quantity);
     }, 0);
     
+    const orderClass = order.complete ? 'Order-Item-Parent Completed' : 'Order-Item-Parent'; 
+    
   return (
-    <div className="Order-Item-Parent" key={order.id} id={order.id}>
+    <div className={orderClass} key={order.id} id={order.id}>
       <h4>Customer: {order.name}</h4>
       <p>{dateDifference(order.date)}</p>
       <p className="order-ID">ID: {order.id}</p>
-      <p>£{totalCost}</p>
+      <p>£{totalCost.toFixed(2)}</p>
       <p>{order.quantity}</p>
       <p>Completed: {order.complete ? 'YES' : 'NO' }</p>
       <p>Completed Date: {order.complete ? <span>{formatDate(order.completedDate)}</span> : <span>n/a</span>}</p>
@@ -41,6 +60,30 @@ const OrderItem = ({
         }
       </p>
       <p>Paid: {order.paid}</p>
+
+      <p className="Product-Eggs">Order requires {requiredEggs || 'n/a'} eggs</p>
+          {requiredEggs && 
+            <>
+              <button className="check-button" onClick={() => {checkOrderPossible(requiredEggs)}}>Check Availability</button>
+
+              {eggsAvailable && 
+                <div className="Eggs-Available">
+                  {eggsAvailable === "NaN" && <></>}
+                  {eggsAvailable >= requiredEggs && 
+                    <h3 className="Available-Green">Order Can Be Completed!</h3>
+                  }
+                  {eggsAvailable < requiredEggs && 
+                    <h3 className="Available-Red">Not Enough Eggs Available Currently!</h3> 
+                  }
+                </div>
+              }
+
+            </>
+          }
+          {!requiredEggs &&
+            <button className="disable-button">Can't Check Order</button>
+          }
+
       <OrdersList orderList={order.order} />
 
       <button onClick={(e)=>{
--- src/components/OrdersList.js
+++ src/components/OrdersList.js
@@ -1,19 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import "./OrdersList.css";
 
 const OrdersList = ({ orderList }) => {
-  const [eggsAvailable, updateEggsAvailable] = useState(null);
-
-  const checkOrderPossible = (number) => {
-      return fetch('https://wt-68dc6486277619b05f4ee73ad2a8a48e-0.sandbox.auth0-extend.com/egg-store-be/quantity')
-        .then(res => res.json())
-        .then(res => {
-          updateEggsAvailable(res.total);
-        })
-        .catch(err => {
-          console.log({error: err});
-        });
-  };
 
   return (
     <section className="Order-List">
@@ -22,27 +10,6 @@ const OrdersList = ({ orderList }) => {
           <h4 className="Product-Name">{orderItem.name}</h4>
           <p className="Product-Price">£{orderItem.price.toFixed(2)}</p>
           <p className="Product-Quantity">Quantity: {orderItem.quantity}</p>
-          
-          <p className="Product-Eggs">Order requires {orderItem.number || 'n/a'} eggs</p>
-          {orderItem.number && 
-            <>
-              <button className="check-button" onClick={() => {checkOrderPossible(orderItem.number)}}>Check Availability</button>
-              {eggsAvailable && 
-                <div className="Eggs-Available">
-                  {eggsAvailable >= orderItem.number && 
-                    <h3 className="Available-Green">Order Can Be Completed!</h3>
-                  }
-                  {eggsAvailable < orderItem.number && 
-                    <h3 className="Available-Red">Not Enough Eggs Available Currently!</h3> 
-                  }
-                </div>
-              }
-            </>
-          }
-          {!orderItem.number &&
-            <button className="disable-button">Can't Check Order</button>
-          }
-
         </div>
       ))}
     </section>
